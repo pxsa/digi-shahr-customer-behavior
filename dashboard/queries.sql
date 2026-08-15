ddcf0a19e78a790bb5dc4d92bb6487ccd26b793d
parsa <paghsis@gmail.com>
2026-08-15 15:56:09 +0330

    add some other queries to metabase

--- dashboard/queries.sql
+++ dashboard/queries.sql
@@ -1,5 +1,5 @@
 --------------------------------------
--- Total Applications
+-- 1) Total Applications
 SELECT
   count(*)
 FROM
@@ -8,7 +8,7 @@ FROM
 
 
 --------------------------------------
---   Overdue Amount
+--  2) Overdue Amount
 SELECT
   sum(i.amount)
 FROM
@@ -22,7 +22,7 @@ WHERE
 
 
 --------------------------------------
---   Approval Rate
+--  3) Approval Rate
 SELECT 
     ROUND(
         COUNT(*) * 100.0 / (SELECT COUNT(*) FROM loan_applications), 
@@ -36,7 +36,7 @@ WHERE status IN ('approved', 'APPROVED');
 
 
 --------------------------------------
--- Loan status
+-- 4) Loan status
 WITH
   normalized_status AS (
     SELECT
@@ -58,9 +58,8 @@ GROUP BY
 --------------------------------------
 
 
-
 --------------------------------------
---   Installment aging report
+--  5) Installment aging report (same as q3)
 SELECT
     CASE
         WHEN r.paid_at IS NOT NULL
@@ -98,3 +97,81 @@ WHERE d.date_gregorian <= DATE '2026-07-15'
 GROUP BY bucket
 ORDER BY total_installment;
 --------------------------------------
+
+
+--------------------------------------
+-- 6) Total Repaid Amound
+SELECT
+  sum(repayments.amount)
+FROM
+  repayments
+
+
+--------------------------------------
+--  7) Repaid Amount per month
+SELECT
+  d.jalali_year,
+  d.jalali_month_name,
+  d.jalali_month,
+  SUM(r.amount) AS paid_amount_per_month
+FROM
+  repayments r
+  JOIN dim_date d ON r.paid_at::date = d.date_gregorian
+GROUP BY
+  d.jalali_year,
+  d.jalali_month_name,
+  d.jalali_month
+ORDER BY
+  d.jalali_year ASC,
+  d.jalali_month
+--------------------------------------
+
+
+--------------------------------------
+--  8) Overdue (1-29 days)
+SELECT
+
+    SUM(i.amount) AS total_amount
+
+FROM installments i
+JOIN dim_date d
+    ON i.due_date = d.date_jalali
+LEFT JOIN repayments r
+    ON i.installment_id = r.installment_id
+
+WHERE r.paid_at IS NULL AND DATE '2026-07-15' - d.date_gregorian BETWEEN 0 AND 29 AND d.date_gregorian <= DATE '2026-07-15'
+--------------------------------------
+
+
+--------------------------------------
+-- 9) Overdue (30 - 59 days)
+SELECT
+
+    SUM(i.amount) AS total_amount
+
+FROM installments i
+JOIN dim_date d
+    ON i.due_date = d.date_jalali
+LEFT JOIN repayments r
+    ON i.installment_id = r.installment_id
+
+WHERE r.paid_at IS NULL AND DATE '2026-07-15' - d.date_gregorian BETWEEN 30 AND 59
+--------------------------------------
+
+
+--------------------------------------
+-- 10) Overdue (+60 days)
+SELECT
+
+    SUM(i.amount) AS total_amount
+
+FROM installments i
+JOIN dim_date d
+    ON i.due_date = d.date_jalali
+LEFT JOIN repayments r
+    ON i.installment_id = r.installment_id
+
+WHERE r.paid_at IS NULL AND DATE '2026-07-15' - d.date_gregorian >= 60
+--------------------------------------
+
+
